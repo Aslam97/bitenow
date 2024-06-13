65b1af5d3332db5c131a0fcdf6e0f97f2a7b0a70
Aslam H <iupin5212@gmail.com>
2024-06-13 12:48:11 +0700

    chore: use ParamProvider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import QueryWrapper from '@/lib/providers/query-wrapper'
+import ParamWrapper from '@/lib/providers/param-wrapper'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <QueryWrapper>{children}</QueryWrapper>
+        <QueryWrapper>
+          <ParamWrapper>{children}</ParamWrapper>
+        </QueryWrapper>
       </body>
     </html>
   )
