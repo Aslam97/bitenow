32686aa8335354bffe5e55031b9f460050f151b2
Aslam H <iupin5212@gmail.com>
2024-06-13 12:48:31 +0700

    chore: set params for page

--- app/page.tsx
+++ app/page.tsx
@@ -1,130 +1,54 @@
-import {
-  ArrowUpDownIcon,
-  ChevronDownIcon,
-  StarIcon,
-  TagIcon
-} from 'lucide-react'
 import Search from './_components/Search'
 import Cuisines from './_components/Cuisines'
 import { Suspense } from 'react'
 import CuisineSkeleton from './_components/CuisineSkeleton'
 import Businesses from './_components/Businesses'
 import BusinessSkeleton from './_components/BusinessSkeleton'
+import BusinesFilter from './_components/filter'
+import type { FilterState } from '@/lib/providers/param-wrapper'
 
 export default async function Home({
   searchParams
 }: {
-  searchParams?: {
-    query?: string
-    cuisine?: string
-  }
+  searchParams: FilterState
 }) {
-  const query = searchParams?.query || ''
-  const cuisine = searchParams?.cuisine || ''
+  const filter = Object.keys(searchParams).reduce((acc, key) => {
+    if (['sort_by', 'page', 'paginate', 'include'].includes(key)) return acc
 
-  console.log('query', query, 'cuisine', cuisine)
+    if (!key || !searchParams[key]) return acc
+    acc[key] = searchParams[key]
+    return acc
+  }, {} as FilterState)
+
+  const sort_by = searchParams.sort_by || '-distance'
+  const pagination = {
+    page: parseInt((searchParams.page as string) || '1'),
+    paginate: parseInt((searchParams.paginate as string) || '20')
+  }
+  const include = searchParams.include || 'cuisines'
 
   return (
     <main className="mx-auto my-6 max-w-7xl space-y-6 px-4 md:px-9 lg:px-10">
-      {/* search */}
+      {/* Search */}
       <Search placeholder="Search for restaurants, dishes, or cuisines" />
 
-      {/* cuisine */}
+      {/* Cuisine */}
       <Suspense fallback={<CuisineSkeleton />}>
         <Cuisines />
       </Suspense>
 
-      {/* filter */}
-      <nav className="no-scrollbar overflow-x-auto">
-        <div
-          aria-label="button group"
-          data-baseweb="button-group"
-          role="group"
-          className="flex space-x-2"
-        >
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            <div className="mr-2 flex">
-              <TagIcon size={16} />
-            </div>
-            Offers
-          </button>
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            <div className="mr-2 flex">
-              <ArrowUpDownIcon size={16} />
-            </div>
-            Sort By
-          </button>
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            Delivery fee
-            <div className="ml-2 flex">
-              <ChevronDownIcon size={16} />
-            </div>
-          </button>
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            Under 30 min
-          </button>
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            <div className="mr-2 flex">
-              <StarIcon size={16} />
-            </div>
-            Rating
-            <div className="ml-2 flex">
-              <ChevronDownIcon size={16} />
-            </div>
-          </button>
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            <div>Price</div>
-            <div className="ml-2 flex">
-              <ChevronDownIcon size={16} />
-            </div>
-          </button>
-          <button
-            tabIndex={0}
-            aria-checked="false"
-            role="checkbox"
-            className="inline-flex h-9 cursor-pointer flex-row items-center justify-center whitespace-nowrap rounded-full bg-zinc-100 px-3 py-2.5 text-sm text-gray-900 transition-all hover:bg-zinc-200"
-          >
-            Dietary
-            <div className="ml-2 flex">
-              <ChevronDownIcon size={16} />
-            </div>
-          </button>
-        </div>
-      </nav>
+      {/* Filter */}
+      <BusinesFilter />
 
-      <Suspense fallback={<BusinessSkeleton />} key={cuisine + query}>
+      {/* Business */}
+      <Suspense fallback={<BusinessSkeleton />} key={JSON.stringify(filter)}>
         <Businesses
-          params={{ term: query, cuisines: cuisine, location: 'a' }}
+          params={{
+            filter,
+            sort_by,
+            pagination,
+            include
+          }}
         />
       </Suspense>
     </main>
