4a6c11dbf74df75b9cdd3480134c87fa19c076b5
Aslam H <iupin5212@gmail.com>
2024-06-15 12:13:44 +0700

    chore: remove query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import QueryWrapper from '@/lib/providers/query-wrapper'
-import ParamWrapper from '@/lib/providers/param-wrapper'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <QueryWrapper>
-          <ParamWrapper>{children}</ParamWrapper>
-        </QueryWrapper>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
