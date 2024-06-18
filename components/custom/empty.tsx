6ec91a65cc76266029edb15921aac80fa9b9be3a
Aslam H <iupin5212@gmail.com>
2024-06-19 00:47:18 +0700

    chore(comp): enhance empty comp

--- components/custom/empty.tsx
+++ components/custom/empty.tsx
@@ -12,18 +12,8 @@ const Empty = ({
     <div
       className={cn('mx-auto flex max-w-md flex-col items-center', className)}
     >
-      <div className="w-56 pb-6">
-        <Image
-          alt=""
-          role="presentation"
-          src="/empty.svg"
-          className="bh ae"
-          width={0}
-          height={0}
-          sizes="100vw"
-          priority
-          style={{ width: '100%', height: '100%' }}
-        />
+      <div className="flex w-56 items-center justify-center pb-6">
+        <Image alt="Empty" src="/empty.png" width={160} height={160} />
       </div>
       <div className="pb-4 text-center text-2xl font-bold">
         {message || 'No data found'}
