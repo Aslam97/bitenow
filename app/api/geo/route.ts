e5c91d5c830b8b4ce525431a9f4704d42164c3ba
Aslam H <iupin5212@gmail.com>
2024-06-19 00:56:02 +0700

    fix(api): use geo ip

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -6,7 +6,7 @@
 
 import { NextRequest, NextResponse } from 'next/server'
 import { getIpFromXForwardedFor } from '@/lib/utils'
-import { getGeoLocation } from '@/lib/utils'
+import { getGeoLocation } from '@/lib/geo-ip-utils'
 
 export async function POST(request: NextRequest) {
   const ips = request.headers.get('x-forwarded-for')
