79abef52208c6dec8826de03ab518c8d00ab02c3
Aslam H <iupin5212@gmail.com>
2024-06-19 00:47:43 +0700

    chore: apply jotai provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { JotaiProvider } from '@/lib/providers/jotai-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={inter.className}>
+        <JotaiProvider>{children}</JotaiProvider>
+      </body>
     </html>
   )
 }
