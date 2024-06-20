85a9f9d3ef032d974704e5ca28463f6c10b30dac
Aslam H <iupin5212@gmail.com>
2024-06-20 13:27:10 +0700

    fix: remove comment

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,9 +1,3 @@
-/**
- * Don't use this in production, use this instead: https://docs.nginx.com/nginx/admin-guide/dynamic-modules/geoip
- * Or use Vercel's geo feature: https://nextjs.org/docs/app/api-reference/functions/next-request#geo
- * Or use a third-party service: https://ip-api.com
- */
-
 import { NextRequest, NextResponse } from 'next/server'
 import { getIpFromXForwardedFor } from '@/lib/utils'
 import { getGeoLocation } from '@/lib/geo-ip-utils'
